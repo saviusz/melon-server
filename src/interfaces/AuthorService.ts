9f84b27818c9042a58e9c1af7bf7a1db58ecfebb
Seweryn Woliński <seweryn.wolinski@tdsoft.com>
2023-11-13 00:51:39 +0100

    Fix(types): Missing promise in AuthorServiece

--- src/interfaces/AuthorService.ts
+++ src/interfaces/AuthorService.ts
@@ -3,5 +3,5 @@ import { Author } from "../models/Author";
 export interface AuthorService {
   getSongAuthors(songId: string): Promise<Author[]>;
   getSongTextAuthors(songId: string): Promise<Author[]>;
-  getAll(): Author[];
+  getAll(): Promise<Author[]>;
 }
