fe7e2aaf7a3a4a42fc9a2ffcfe9fed9ebdd03203
Seweryn Woliński <42438445+saviusz@users.noreply.github.com>
2023-12-28 01:21:31 +0100

    refactor: Add TitlesRepository tests, resolve fails
    Resolve #23
    
    Signed-off-by: Seweryn Woliński <42438445+saviusz@users.noreply.github.com>

--- tests/repositories/titles.test.ts
+++ tests/repositories/titles.test.ts
@@ -1,4 +1,4 @@
-import { describe } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import Knex from "knex";
 import knexfile from "../../knexfile";
@@ -10,13 +10,65 @@ import { KnexTitlesRepository } from "../../src/repositories/Titles/TitlesReposi
 const database = Knex(knexfile["test"]);
 up(database);
 
-describe.todo.each([
+describe.each([
   { name: "Knex", repo: new KnexTitlesRepository(database) },
   { name: "Dummy", repo: new DummyTitlesRepository() }
 ])("$name Titles Repo", ({ repo }: { repo: ITitlesRepository }) => {
-  describe("when adding with valid data", () => {
+  describe("when adding one with valid data", () => {
+    const validTitle = "tytul";
+    const validSongId = "songId";
+    it("should return provided title", async () => {
+      // Act
+      const res = await repo.add(validSongId, validTitle);
 
-    // TODO: Add this test
+      // Assert
+      expect(res).toBe(validTitle);
+    });
+
+    it("should be included in titles of song", async () => {
+      // Act
+      const res = await repo.getOnSong(validSongId);
+
+      // Assert
+      expect(res).toContain(validTitle);
+    });
+
+    it("should create songId in list of all ids", async () => {
+      // Act
+      const res = await repo.getSongsIds();
+
+      // Assert
+      expect(res).toContain(validSongId);
+    });
+
+  });
+
+  describe("when adding multiple with valid data", () => {
+    const validTitles = [ "Pieśń rozdartego serca", "Aezakmi" ];
+    const validSongId = "songId";
+    it("should return provided titles", async () => {
+      // Act
+      const res = await repo.addMany(validSongId, validTitles);
+
+      // Assert
+      expect(res).toEqual(validTitles);
+    });
+
+    it("should be included in titles of song", async () => {
+      // Act
+      const res = await repo.getOnSong(validSongId);
+
+      // Assert
+      expect(res).toEqual(expect.arrayContaining(validTitles));
+    });
+
+    it("should create songId in list of all ids", async () => {
+      // Act
+      const res = await repo.getSongsIds();
+
+      // Assert
+      expect(res).toContain(validSongId);
+    });
 
   });
 });
