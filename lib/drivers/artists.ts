89dcddea0aadadb88b3b72f2f00e65af75edcfde
Seweryn Woliński <42438445+saviusz@users.noreply.github.com>
2025-09-22 14:34:48 +0200

    feat: add ArtistSelector driver

--- lib/drivers/artists.ts
+++ lib/drivers/artists.ts
@@ -8,7 +8,6 @@ interface Props {
   surname: string | null;
 }
 
-export type ArtistInserter = (data: Props) => Result<ArtistRow, unknown>;
 type ArtistRow = {
   id: number;
   name: string | null;
@@ -16,6 +15,7 @@ type ArtistRow = {
   pseudonym: string | null;
 };
 
+export type ArtistInserter = (data: Props) => Result<ArtistRow, unknown>;
 export function createArtistInserter(sql: QueryFn): ArtistInserter {
   return async (data: Props): Result<ArtistRow, unknown> => {
     const response = await sql<ArtistRow>`
@@ -31,6 +31,24 @@ export function createArtistInserter(sql: QueryFn): ArtistInserter {
 
     return {
       data: response.data!.rows[0]!,
+      error: null,
+    };
+  };
+}
+
+export type ArtistSelector = () => Result<ArtistRow[], unknown>;
+export function createArtistSelector(sql: QueryFn): ArtistSelector {
+  return async () => {
+    const { data, error } =
+      await sql<ArtistRow>`SELECT artist_id, name, surname, pseudonym FROM artists`;
+    if (error || !data?.rows)
+      return {
+        data: null,
+        error: "Couldn't select authors",
+      };
+
+    return {
+      data: data?.rows,
       error: null,
     };
   };
